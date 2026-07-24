5157802263a2dfad54cbf3f435320ba16d14de2f
Kelyzor <kelyzor25@gmail.com>
2026-07-24 20:15:05 +1000

    Time: 0 ms (100%), Space: 9.8 MB (23.81%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -3,8 +3,8 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         size_t count = 0;
         for (size_t i = 0; i < size(nums); i++) {
-            for (size_t j = 0; j < size(nums); j++) {
-                if (nums[i] == nums[j] && i < j) {
+            for (size_t j = i + 1; j < size(nums); j++) {
+                if (nums[i] == nums[j]) {
                     count++;
                 }
             }
